df14ba1e0e8cadfe3e8633e499c66f4d537a88f2
ark948 <armankarimi948@gmail.com>
2025-04-22 13:09:43 +0330

    Adding login without refresh (halfway through)

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signup/', views.signup_user, name='signup'),
+    path('logout', views.logout_user, name='logout'),
+    path('login/', views.login_user, name='login'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+
+from .models import CustomUser
 
 # Create your views here.
 
@@ -7,3 +14,29 @@ from django.http.response import HttpResponse
 
 def index(request):
     return HttpResponse('accounts index')
+
+
+
+@require_http_methods(['POST'])
+def signup_user(request: HttpRequest):
+    pass
+
+
+@require_http_methods(['POST'])
+def login_user(request: HttpRequest):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+    else:
+        print("\n\nWRONG CREDENTIALS\n\n")
+    context = { 'user': user }
+    resposne = render(request, 'accounts/partials/user-info-partial.html', context=context)
+    resposne['HX-Trigger'] = 'success'
+    return resposne
+
+
+def logout_user(request: HttpRequest):
+    if request.method == "POST":
+        logout(request)
