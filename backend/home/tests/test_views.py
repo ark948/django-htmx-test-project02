4aaf575d961cb7a71102645d3943e0db0e086338
ark948 <armankarimi948@gmail.com>
2025-04-26 14:28:54 +0330

    A bit on more tests

--- backend/home/tests/test_views.py
+++ backend/home/tests/test_views.py
@@ -1,9 +1,11 @@
 import pytest
 from django.test.client import Client
 from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
 
 @pytest.mark.django_db
 def test_home_index(client: Client):
     response = client.get(reverse("home:index"))
     assert response.status_code == 200
+    assertTemplateUsed(response, "home/index.html")
