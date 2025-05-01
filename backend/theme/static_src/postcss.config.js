44661843c40f21af12bd44cd086dab8b388441a8
ark948 <armankarimi948@gmail.com>
2025-05-01 21:04:38 +0330

    Fixed alert colors

--- backend/theme/static_src/postcss.config.js
+++ backend/theme/static_src/postcss.config.js
@@ -4,4 +4,10 @@ module.exports = {
     "postcss-simple-vars": {},
     "postcss-nested": {}
   },
+  safelist: [
+    'alert-info',
+    'alert-success',
+    'alert-warning',
+    'alert-error',
+  ],
 }
