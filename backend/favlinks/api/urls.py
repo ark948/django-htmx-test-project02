4ff519818e6ea339dd8b649037901d53b1906e32
ark948 <armankarimi948@gmail.com>
2025-06-02 16:34:21 +0330

    Create website OK (favlinks)

--- backend/favlinks/api/urls.py
+++ backend/favlinks/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("websites/list/", views.WebsiteView.as_view(), name='api-website-list'),
     path('', views.api_index, name='api-index'),
 ]
--- backend/favlinks/api/views.py
+++ backend/favlinks/api/views.py
@@ -1,8 +1,41 @@
+from django.db.models import QuerySet
 from django.http.request import HttpRequest
 from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
+from . import serializers
+from favlinks.models import Website
 
 
 def api_index(request: HttpRequest) -> Response:
     return JsonResponse({'message': "API Index Ok."})
+
+
+# listCreateAPIView
+# RetrieveUpdateDelete
+
+
+class WebsiteView(generics.ListCreateAPIView):
+    serializer_class = serializers.WebsiteModelSerializer
+    permission_classes = (
+        IsAuthenticated,
+    )
+
+    def get_queryset(self, *args, **kwargs) -> QuerySet:
+        return Website.objects.filter( user = self.request.user )
+    
+    def post(self, request: HttpRequest, *args, **kwargs):
+        data = {
+            'title' : request.data.get('title'),
+            'url': request.data.get('url'),
+            'username': request.data.get('username'),
+            'password': request.data.get('password'),
+        }
+
+        serializer = serializers.WebsiteModelSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save(user = request.user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
