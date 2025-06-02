f4ff7c52d808eda1878c8fd4511ce7a135a44e99
ark948 <armankarimi948@gmail.com>
2025-06-02 16:11:40 +0330

    Added Website Model Serializer (favlinks)

---
+++ backend/favlinks/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from favlinks.models import Website, Link, Category
+
+class WebsiteModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Website
+        exclude = (
+            'id',
+            'user',
+        )
