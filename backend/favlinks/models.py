85e6982a7a8f95397481ae7fee862e54483174b7
ark948 <armankarimi948@gmail.com>
2025-06-02 15:44:15 +0330

    Added Category model to favlinks app

--- backend/favlinks/models.py
+++ backend/favlinks/models.py
@@ -3,8 +3,14 @@ from django.db import models
 from encrypted_fields.fields import EncryptedCharField
 
 from accounts.models import CustomUser
+from home.models import BaseModel
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=40, null=True, blank=True)
 
 
 class Website(models.Model):
@@ -12,7 +18,7 @@ class Website(models.Model):
     url = models.URLField(verbose_name="Address", unique=True, null=False, blank=False)
     username = models.CharField(verbose_name="Username", max_length=68, null=True, blank=True)
     password = EncryptedCharField(verbose_name="Password", null=True, blank=True)
-    user = models.ForeignKey( CustomUser, on_delete=models.CASCADE, related_name='websites') # user.websites.all()
+    user = models.ForeignKey( CustomUser, on_delete=models.CASCADE, related_name='websites' ) # user.websites.all()
 
     class Meta:
         verbose_name_plural = "Websites"
@@ -28,7 +34,8 @@ class Website(models.Model):
 class Link(models.Model):
     url = models.URLField(verbose_name="Address", null=False, blank=False)
     description = models.CharField(verbose_name="Description", null=True, blank=True)
-    website = models.ForeignKey(Website, on_delete=models.CASCADE, related_name='links') # website.links.all()
+    category = models.ForeignKey( Category, on_delete=models.DO_NOTHING, related_name='links', null=True ) # category.links.all()
+    website = models.ForeignKey( Website, on_delete=models.CASCADE, related_name='links' ) # website.links.all()
 
     class Meta:
         verbose_name_plural = "Links"
