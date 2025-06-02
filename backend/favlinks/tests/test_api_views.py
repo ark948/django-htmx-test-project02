35ca238cd12dcc1b89b5684ef0ba0e2791e5dcbc
ark948 <armankarimi948@gmail.com>
2025-06-02 22:55:37 +0330

    Added two more tests (favlinks)

--- backend/favlinks/tests/test_api_views.py
+++ backend/favlinks/tests/test_api_views.py
@@ -8,3 +8,24 @@ def test_favlinks_api_index(client: APIClient):
     response = client.get(reverse("favlinks:api-index"))
 
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_favlinks_api_websites_list_is_empty(user, client: APIClient):
+    client.force_login(user)
+    response = client.get(reverse("favlinks:api-website-list"))
+
+    assert response.status_code == 200
+    assert response.data == []
+
+
+@pytest.mark.django_db
+def test_favlinks_api_websites_list_not_empty(user, user_with_one_website, client: APIClient):
+    client.force_login(user)
+    response = client.get(reverse("favlinks:api-website-list"))
+
+    assert response.status_code == 200
+    assert response.data != []
+    assert response.data[0]['title'] == user_with_one_website.title
+    assert response.data[0]['url'] == user_with_one_website.url
+    assert len(response.data) == 1
