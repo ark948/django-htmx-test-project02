1d35d168bd5a918702bf1e1a1d1a2478108c6cfb
ark948 <armankarimi948@gmail.com>
2025-06-06 22:12:26 +0330

    incomplete view for favlinks

--- backend/favlinks/views.py
+++ backend/favlinks/views.py
@@ -7,3 +7,6 @@ from django.http.response import HttpResponse
 # test ok
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "favlinks/index.html")
+
+def create_new_website(request: HttpRequest) -> HttpResponse:
+    return render(request, "favlinks/new.html", {})
